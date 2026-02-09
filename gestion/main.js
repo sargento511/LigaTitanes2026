240910b2b0de286b3224fa23386ca1928e80fa73
sargento511 <sargento511sargento@gmail.com>
2026-02-08 19:50:49 -0600

    Update main.js

--- gestion/main.js
+++ gestion/main.js
@@ -7,36 +7,36 @@ const datosEquipos = {
             { nombre: 'Esperando lista...', valor: 0, salario: 0, prima: 0, enVenta: false }
         ]
     },
-    'Halcones': {
-        nombre: 'HALCONES ROJOS',
-        saldo: 276.4,
-        estadio: 'La Caldera Roja (Gigante)',
-        jugadores: [
-            { nombre: 'Keylor Navas', valor: 0.8, salario: 0.8, prima: 0.4, enVenta: false },
-            { nombre: 'Puchacz', valor: 1.5, salario: 1.5, prima: 0.7, enVenta: false },
-            { nombre: 'Kimpembe', valor: 4, salario: 8, prima: 2, enVenta: false },
-            { nombre: 'Yan Couto', valor: 20, salario: 5, prima: 1.5, enVenta: false },
-            { nombre: 'David Raum', valor: 20, salario: 5, prima: 1.5, enVenta: false },
-            { nombre: 'DeAndre Yedlin', valor: 1, salario: 1, prima: 0.4, enVenta: false },
-            { nombre: 'Yeray Álvarez', valor: 1, salario: 1, prima: 0.4, enVenta: false },
-            { nombre: 'Unai Simón', valor: 25, salario: 8, prima: 2, enVenta: false },
-            { nombre: 'Luis Alberto', valor: 5, salario: 5, prima: 0.7, enVenta: false },
-            { nombre: 'Pape Cissé', valor: 1, salario: 1, prima: 0.4, enVenta: false },
-            { nombre: 'Granit Xhaka', valor: 10, salario: 5, prima: 1.5, enVenta: false },
-            { nombre: 'Trindade', valor: 28, salario: 8, prima: 2, enVenta: false },
-            { nombre: 'Tomáš Souček', valor: 12, salario: 5, prima: 1.5, enVenta: false },
-            { nombre: 'Gilberto Mora', valor: 10, salario: 5, prima: 1.5, enVenta: false },
-            { nombre: 'Paul Pogba', valor: 5, salario: 6, prima: 2, enVenta: false },
-            { nombre: 'Daniel James', valor: 14, salario: 14, prima: 4, enVenta: false },
-            { nombre: 'Samuel Chukwueze', valor: 10, salario: 5, prima: 1.5, enVenta: false },
-            { nombre: 'Kaoru Mitoma', valor: 30, salario: 11, prima: 3, enVenta: false },
-            { nombre: 'Antonio Nusa', valor: 32, salario: 11, prima: 3, enVenta: false },
-            { nombre: 'Takefusa Kubo', valor: 30, salario: 11, prima: 3, enVenta: false },
-            { nombre: 'Youssoufa Moukoko', valor: 7, salario: 5, prima: 1.5, enVenta: false },
-            { nombre: 'Victor Osimhen', valor: 10, salario: 15, prima: 5, enVenta: false },
-            { nombre: 'Aymeric Laporte', valor: 9, salario: 7, prima: 2, enVenta: false }
-        ]
-    }
+   'Halcones': {
+    nombre: 'HALCONES ROJOS',
+    saldo: 276.4,
+    estadio: 'La Caldera Roja (Gigante)',
+    jugadores: [
+        { nombre: 'Keylor Navas', valor: 0.8, salario: 0.8, prima: 0.4, enVenta: false, contrato: 2 },
+        { nombre: 'Puchacz', valor: 1.5, salario: 1.5, prima: 0.7, enVenta: false, contrato: 2 },
+        { nombre: 'Kimpembe', valor: 4, salario: 8, prima: 2, enVenta: false, contrato: 2 },
+        { nombre: 'Yan Couto', valor: 20, salario: 5, prima: 1.5, enVenta: false, contrato: 2 },
+        { nombre: 'David Raum', valor: 20, salario: 5, prima: 1.5, enVenta: false, contrato: 2 },
+        { nombre: 'DeAndre Yedlin', valor: 1, salario: 1, prima: 0.4, enVenta: false, contrato: 2 },
+        { nombre: 'Yeray Álvarez', valor: 1, salario: 1, prima: 0.4, enVenta: false, contrato: 2 },
+        { nombre: 'Unai Simón', valor: 25, salario: 8, prima: 2, enVenta: false, contrato: 2 },
+        { nombre: 'Luis Alberto', valor: 5, salario: 5, prima: 0.7, enVenta: false, contrato: 2 },
+        { nombre: 'Pape Cissé', valor: 1, salario: 1, prima: 0.4, enVenta: false, contrato: 2 },
+        { nombre: 'Granit Xhaka', valor: 10, salario: 5, prima: 1.5, enVenta: false, contrato: 2 },
+        { nombre: 'Trindade', valor: 28, salario: 8, prima: 2, enVenta: false, contrato: 2 },
+        { nombre: 'Tomáš Souček', valor: 12, salario: 5, prima: 1.5, enVenta: false, contrato: 2 },
+        { nombre: 'Gilberto Mora', valor: 10, salario: 5, prima: 1.5, enVenta: false, contrato: 2 },
+        { nombre: 'Paul Pogba', valor: 5, salario: 6, prima: 2, enVenta: false, contrato: 2 },
+        { nombre: 'Daniel James', valor: 14, salario: 14, prima: 4, enVenta: false, contrato: 2 },
+        { nombre: 'Samuel Chukwueze', valor: 10, salario: 5, prima: 1.5, enVenta: false, contrato: 2 },
+        { nombre: 'Kaoru Mitoma', valor: 30, salario: 11, prima: 3, enVenta: false, contrato: 2 },
+        { nombre: 'Antonio Nusa', valor: 32, salario: 11, prima: 3, enVenta: false, contrato: 2 },
+        { nombre: 'Takefusa Kubo', valor: 30, salario: 11, prima: 3, enVenta: false, contrato: 2 },
+        { nombre: 'Youssoufa Moukoko', valor: 7, salario: 5, prima: 1.5, enVenta: false, contrato: 2 },
+        { nombre: 'Victor Osimhen', valor: 10, salario: 15, prima: 5, enVenta: false, contrato: 2 },
+        { nombre: 'Aymeric Laporte', valor: 9, salario: 7, prima: 2, enVenta: false, contrato: 2 }
+    ]
+}
 };
 
 let equipoActual = null;
